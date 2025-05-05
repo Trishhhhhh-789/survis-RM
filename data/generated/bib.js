f273349d31f53171aa447e09257add17e45a7f0c
Trishhhhhh-789 <cuichijie@126.com>
2025-05-05 21:44:09 +0800

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,94 @@
+﻿const generatedBibEntries = {
+    "bai2024lownoisebounds": {
+        "author": " Anonymous ",
+        "journal": " Unpublished ",
+        "keywords": " Generalization Bound, Low-noise, IMC ",
+        "series": " None",
+        "title": " Generalization Bounds for Inductive Matrix Completion in Low-noise Settings ",
+        "type": "misc",
+        "year": " 2024 "
+    },
+    "hao2019drugrepositioning": {
+        "author": " Yunda Hao and Menglan Cai and Limin Li ",
+        "doi": " 10.1049/iet-syb.2018.5129 ",
+        "journal": " IET Systems Biology ",
+        "keywords": " Drug-target prediction, Matrix Completion, Multi-view learning ",
+        "series": " Vol. 13",
+        "title": " Drug repositioning via matrix completion with multi-view side information ",
+        "type": "article",
+        "year": " 2019 "
+    },
+    "ledent2021finegrained": {
+        "author": " Antoine Ledent and Rodrigo Alves and Yunwen Lei and Marius Kloft ",
+        "journal": " NeurIPS ",
+        "keywords": " AUC, Inductive Matrix Completion, Generalization ",
+        "series": " NeurIPS 2021",
+        "title": " Fine-grained Generalisation Analysis of Inductive Matrix Completion ",
+        "type": "inproceedings",
+        "year": " 2021 "
+    },
+    "li2024gae": {
+        "author": " Anonymous ",
+        "journal": " Unpublished ",
+        "keywords": " Graph Autoencoder, IMC, Link Prediction ",
+        "series": " None",
+        "title": " Inductive Matrix Completion Using Graph Autoencoder ",
+        "type": "misc",
+        "year": " 2024 "
+    },
+    "liu2024biomatrix": {
+        "author": " Anonymous ",
+        "journal": " Unpublished ",
+        "keywords": " Biodiversity, Matrix Completion, IMC ",
+        "series": " None",
+        "title": " Biological Diversity Enhanced Inductive Matrix Completion ",
+        "type": "misc",
+        "year": " 2024 "
+    },
+    "liu2024gcnbio": {
+        "author": " Anonymous ",
+        "journal": " Unpublished ",
+        "keywords": " Bioinformatics, GCN, IMC ",
+        "series": " None",
+        "title": " Graph-based Bioinformatics Prediction Using IMC ",
+        "type": "misc",
+        "year": " 2024 "
+    },
+    "ross2024hybrid": {
+        "author": " Anonymous ",
+        "journal": " Unpublished ",
+        "keywords": " Hybrid models, Graph Learning, IMC ",
+        "series": " None",
+        "title": " Hybrid Inductive Graph Method ",
+        "type": "misc",
+        "year": " 2024 "
+    },
+    "wang2024aeimc": {
+        "author": " YaWei Wang and ZhiXiang Yin ",
+        "doi": " 10.1038/s41598-024-78212-w ",
+        "journal": " Scientific Reports ",
+        "keywords": " IMC, miRNA, disease prediction ",
+        "series": " Vol. 14",
+        "title": " Prediction of miRNA-disease association based on multisource inductive matrix completion ",
+        "type": "article",
+        "year": " 2024 "
+    },
+    "zhang2024inductivegcn": {
+        "author": " Anonymous ",
+        "journal": " Unpublished ",
+        "keywords": " GNN, IMC, representation learning ",
+        "series": " None",
+        "title": " Inductive Matrix Completion Based on Graph Neural Networks ",
+        "type": "misc",
+        "year": " 2024 "
+    },
+    "zhou2024metalearning": {
+        "author": " Anonymous ",
+        "journal": " Unpublished ",
+        "keywords": " Meta-learning, IMC, Drug Discovery ",
+        "series": " None",
+        "title": " Meta-learning-based Inductive Logistic Matrix Completion for Prediction of Kinase Inhibitors ",
+        "type": "misc",
+        "year": " 2024 "
+    }
+};
